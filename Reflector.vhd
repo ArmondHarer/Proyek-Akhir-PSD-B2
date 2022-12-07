2baea688fb5575534a74d25047f3f9aa6417a728
irbirojodoh <greget007.ir@gmail.com>
2022-12-07 11:13:03 +0700

    Reflector added

--- Reflector.vhd
+++ Reflector.vhd
@@ -1,47 +1,72 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
-USE ieee.numeric_std.ALL;
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
 
-ENTITY Reflector IS
-    PORT (
-        clk : IN STD_LOGIC;
-        isPressed : IN STD_LOGIC;
+entity Reflector is
+    Port ( clk : in std_logic;
+        turnNext : in std_logic;
+        direction: in std_logic; --0 untuk kanan ke kiri, 1 untuk kiri ke kanan
+        isPressed : in std_logic;
+        notchPos : in std_logic;
+        rotorPos: in integer;
         --Input and outputs are in ASCII binary
-        letterIn : INOUT STD_LOGIC_VECTOR(7 DOWNTO 0);
-        letterOut : INOUT STD_LOGIC_VECTOR(7 DOWNTO 0));
-END ENTITY;
+        letterin : inout std_logic_vector(7 downto 0);
+        letterout : inout std_logic_vector(7 downto 0));
+end rotor1;
 
-ARCHITECTURE rtl OF Reflector IS
-BEGIN
-    PROCESS (letterIn)
-    BEGIN
-        CASE letterIn IS
-            WHEN "01000001" => letterOut <= "01000101";
-            WHEN "01000010" => letterOut <= "01001011";
-            WHEN "01000011" => letterOut <= "01001101";
-            WHEN "01000100" => letterOut <= "01000110";
-            WHEN "01000101" => letterOut <= "01001100";
-            WHEN "01000110" => letterOut <= "01000111";
-            WHEN "01000111" => letterOut <= "01000100";
-            WHEN "01001000" => letterOut <= "01010001";
-            WHEN "01001001" => letterOut <= "01010110";
-            WHEN "01001010" => letterOut <= "01011010";
-            WHEN "01001011" => letterOut <= "01001110";
-            WHEN "01001100" => letterOut <= "01010100";
-            WHEN "01001101" => letterOut <= "01001111";
-            WHEN "01001110" => letterOut <= "01010111";
-            WHEN "01001111" => letterOut <= "01011001";
-            WHEN "01010000" => letterOut <= "01001000";
-            WHEN "01010001" => letterOut <= "01011000";
-            WHEN "01010010" => letterOut <= "01010101";
-            WHEN "01010011" => letterOut <= "01010011";
-            WHEN "01010100" => letterOut <= "01010000";
-            WHEN "01010101" => letterOut <= "01000001";
-            WHEN "01010110" => letterOut <= "01001001";
-            WHEN "01010111" => letterOut <= "01000010";
-            WHEN "01011000" => letterOut <= "01010010";
-            WHEN "01011001" => letterOut <= "01000011";
-            WHEN "01011010" => letterOut <= "01001010";
-        END CASE;
-    END PROCESS;
-END ARCHITECTURE;
+
+architecture rtl of rotor1 is
+
+    
+begin
+    
+
+
+
+    process (isPressed)
+    begin
+        if direction = '1' then  
+            case letterin is
+                --IN    ABCDEFGHIJKLMNOPQRSTUVWXYZ
+                --OUT   EJMZALYXVBWFCRQUONTSPIKHGD
+            
+                when "01000001" => letterout <= "01000101"; 
+                when "01000010" => letterout <= "01001010";
+                when "01000011" => letterout <= "01001101";
+                when "01000100" => letterout <= "01011010";
+                when "01000101" => letterout <= "01000001";
+                when "01000110" => letterout <= "01001100";
+                when "01000111" => letterout <= "01011001";
+                when "01001000" => letterout <= "01011000";
+                when "01001001" => letterout <= "01010110";
+                when "01001010" => letterout <= "01000010";
+                when "01001011" => letterout <= "01010111";
+                when "01001100" => letterout <= "01000110";
+                when "01001101" => letterout <= "01000011";
+                when "01001110" => letterout <= "01010010";
+                when "01001111" => letterout <= "01010001";
+                when "01010000" => letterout <= "01010101";
+                when "01010001" => letterout <= "01001111";
+                when "01010010" => letterout <= "01001110";
+                when "01010011" => letterout <= "01010100";
+                when "01010100" => letterout <= "01010011";
+                when "01010101" => letterout <= "01010000";
+                when "01010110" => letterout <= "01001001";
+                when "01010111" => letterout <= "01001011";
+                when "01011000" => letterout <= "01001000";
+                when "01011001" => letterout <= "01000111";
+                when "01011010" => letterout <= "01000100";
+                when others => report "Out of bounds" severity failure;
+            end case;
+        
+            
+    end process;
+      
+            
+
+
+
+    
+end architecture rtl;
+
+
