6173f1a462ec1c4c5df6fc4a5907051507d1d50f
ArmondHarer <88547347+ArmondHarer@users.noreply.github.com>
2022-12-10 00:16:44 +0700

    More minor changes to Plugboard.vhd
    IDK if this would solve the problem, please give it a go and tell me if it works (or if it doesn't)

--- Plugboard.vhd
+++ Plugboard.vhd
@@ -9,22 +9,18 @@ entity Plugboard is
     port (
         clk   : in std_logic;
         readwrite : in std_logic;
-        letter_in  : in  std_logic_vector(7 downto 0); 
-        letter_write  : in  std_logic_vector(7 downto 0);
-
-	    
+        letter_in  : in  std_logic_vector(7 downto 0) := "01000001";
 	letter_out : out std_logic_vector(7 downto 0)
     );
 end entity Plugboard;
 
 architecture Behavioral of Plugboard is
-        signal indexnum : integer;
+        signal indexnum : integer := 1;
         constant plugboardConst :PLUGBOARD_CONFIG := PLUGBOARDMAP;
 begin
 
     digantibiargeror : process(clk)
 	begin
-        indexnum <= 1;
 		indexnum <= to_integer(unsigned(letter_in)) mod 65;
 		letter_out <= plugboardConst(indexnum);
 	end process;
