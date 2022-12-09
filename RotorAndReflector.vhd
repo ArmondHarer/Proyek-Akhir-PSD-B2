d29d5aa4b91288d4793386787cbf99659798967a
Althaf Nafi <althafnaa@gmail.com>
2022-12-09 19:51:16 +0700

    finished rotor and reflector not polished

--- RotorAndReflector.vhd
+++ RotorAndReflector.vhd
@@ -5,22 +5,64 @@ USE ieee.numeric_std.ALL;
 ENTITY RotorAndReflector IS
     PORT (
         input : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-        output_rotor : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+        outputRotorRight : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+        outputRotorMid : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+        outputRotorLeft : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+        outputReflector : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+        outputRevRotorLeft : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+        outputRevRotorMid : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
         output : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
     );
 END ENTITY;
 
 ARCHITECTURE rtl OF RotorAndReflector IS
+    SIGNAL rotorBufferRight : STD_LOGIC_VECTOR(7 DOWNTO 0);
+    SIGNAL rotorBufferMid : STD_LOGIC_VECTOR(7 DOWNTO 0);
+    SIGNAL rotorBufferLeft : STD_LOGIC_VECTOR(7 DOWNTO 0);
     SIGNAL reflectorBuffer : STD_LOGIC_VECTOR(7 DOWNTO 0);
+    SIGNAL revRotorBufferRight : STD_LOGIC_VECTOR(7 DOWNTO 0);
+    SIGNAL revRotorBufferMid : STD_LOGIC_VECTOR(7 DOWNTO 0);
+    SIGNAL revRotorBufferLeft : STD_LOGIC_VECTOR(7 DOWNTO 0);
 BEGIN
     -- Rightmost rotor component
     rotor_right : ENTITY work.Rotor
         GENERIC MAP(initialPos => 1, notchPos => 0, firstRotor => '1')
-        PORT MAP(rotor_type => "00", direction => '1', letterIn => input, letterOut => reflectorBuffer);
-    -- Save output from first rotor to output_rotor
-    output_rotor <= reflectorBuffer;
+        PORT MAP(rotor_type => "00", direction => '0', letterIn => input, letterOut => rotorBufferRight);
+    outputRotorRight <= rotorBufferRight;
+
+    -- Middle rotor component
+    rotor_middle : ENTITY work.Rotor
+        GENERIC MAP(initialPos => 1, notchPos => 0, firstRotor => '0')
+        PORT MAP(rotor_type => "01", direction => '0', letterIn => rotorBufferRight, letterOut => rotorBufferMid);
+    outputRotorMid <= rotorBufferMid;
+
+    -- Left rotor component
+    rotor_left : ENTITY work.Rotor
+        GENERIC MAP(initialPos => 1, notchPos => 0, firstRotor => '0')
+        PORT MAP(rotor_type => "10", direction => '0', letterIn => rotorBufferMid, letterOut => rotorBufferLeft);
+    outputRotorLeft <= rotorBufferLeft;
+
     -- The reflector component
     reflector_unit : ENTITY work.Reflector
-        PORT MAP(letterin => reflectorBuffer, letterout => output);
+        PORT MAP(letterin => rotorBufferLeft, letterout => reflectorBuffer);
+    outputReflector <= reflectorBuffer;
+
+    -- Reverse left rotor component
+    rev_rotor_left : ENTITY work.Rotor
+        GENERIC MAP(initialPos => 1, notchPos => 0, firstRotor => '0')
+        PORT MAP(rotor_type => "10", direction => '1', letterIn => reflectorBuffer, letterOut => revRotorBufferLeft);
+    outputRevRotorLeft <= revRotorBufferLeft;
+
+    -- Reverse middle rotor component
+    rev_rotor_middle : ENTITY work.Rotor
+        GENERIC MAP(initialPos => 1, notchPos => 0, firstRotor => '0')
+        PORT MAP(rotor_type => "01", direction => '1', letterIn => revRotorBufferLeft, letterOut => revRotorBufferMid);
+    outputRevRotorMid <= revRotorBufferMid;
+
+    -- Reverse rightmost rotor component
+    rev_rotor_right : ENTITY work.Rotor
+        GENERIC MAP(initialPos => 1, notchPos => 0, firstRotor => '1')
+        PORT MAP(rotor_type => "00", direction => '1', letterIn => revRotorBufferMid, letterOut => revRotorBufferRight);
+    output <= revRotorBufferRight;
 
 END ARCHITECTURE;
--- RotorAndReflector_tb.vhd
+++ RotorAndReflector_tb.vhd
@@ -9,29 +9,43 @@ ARCHITECTURE rtl OF RotorAndReflector_tb IS
     COMPONENT RotorAndReflector IS
         PORT (
             input : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-            output_rotor : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+            outputRotorRight : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+            outputRotorMid : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+            outputRotorLeft : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+            outputReflector : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+            outputRevRotorLeft : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+            outputRevRotorMid : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
             output : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
         );
     END COMPONENT;
     SIGNAL INP : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL OUTP_ROTOR : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL OUTP_ROTOR_RIGHT : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL OUTP_ROTOR_MID : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL OUTP_ROTOR_LEFT : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL OUTP_REV_ROTOR_RIGHT : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL OUTP_REV_ROTOR_MID : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL OUTP_REV_ROTOR_LEFT : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
     SIGNAL OUTP : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
 BEGIN
     UUT : RotorAndReflector
-    PORT MAP(input => INP, output_rotor => OUTP_ROTOR, output => OUTP);
+    PORT MAP(
+        input => INP,
+        outputRotorRight => OUTP_ROTOR_RIGHT,
+        outputRotorMid => OUTP_ROTOR_MID,
+        outputRotorLeft => OUTP_ROTOR_LEFT,
+        outputReflector => OUTP_REV_ROTOR_RIGHT,
+        outputRevRotorLeft => OUTP_REV_ROTOR_LEFT,
+        outputRevRotorMid => OUTP_REV_ROTOR_MID,
+        output => OUTP
+    );
 
     tb_proc : PROCESS
-        VARIABLE TEMP_OUT : STD_LOGIC_VECTOR(7 DOWNTO 0);
-        -- VARIABLE INP : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
-        -- VARIABLE OUTP_ROTOR : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
-        -- VARIABLE OUTP : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
     BEGIN
         INP <= "01001011";
         WAIT FOR 10 ps;
         FOR i IN 65 TO 90 LOOP
             REPORT "i=" & INTEGER'image(i);
             INP <= STD_LOGIC_VECTOR(to_unsigned(i, INP'length));
-            TEMP_OUT := OUTP_ROTOR;
             WAIT FOR 10 ps;
         END LOOP;
         WAIT;
